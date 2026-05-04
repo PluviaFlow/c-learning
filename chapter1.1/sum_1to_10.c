9977e907579740789932723318e55bbfc9addf42
PluviaFlow <PluviaFlow@outlook.com>
2026-05-04 20:53:52 +0800

    Day01

---
+++ chapter1.1/sum_1to_10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    int i = 0;
+    int sum = 0;
+
+    for(i = 1; i <= 10; i++)
+    {
+        sum = sum+i;
+        }
+
+    printf("%d\n",sum);
+
+    return 0;
+}
